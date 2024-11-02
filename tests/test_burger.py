5ac14d4198ba18275cc3fcce33e19dbc0ecdc631
Eldar Dikopp <eldar.dikopp@yandex.ru>
2024-11-02 14:21:07 +0300

    v 0.9 added mock

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -2,6 +2,7 @@ from praktikum.burger import Burger
 from praktikum.bun import Bun
 from praktikum.ingredient import Ingredient
 from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE
+from unittest.mock import Mock
 
 
 def test_set_bun_success():
@@ -40,10 +41,12 @@ def test_move_ingredient_success():
 
 def test_get_price_success():
     burger = Burger()
-    bun = Bun("black bun", 100)
-    burger.set_buns(bun)
-    ingredient = Ingredient(INGREDIENT_TYPE_SAUCE, "hot sauce", 100)
-    burger.add_ingredient(ingredient)
+    bun_mock = Mock()
+    bun_mock.get_price.return_value = 100
+    burger.set_buns(bun_mock)
+    ingredient_mock = Mock()
+    ingredient_mock.get_price.return_value = 100
+    burger.add_ingredient(ingredient_mock)
     assert burger.get_price() == 300
 
 
--- tests/test_ingredient.py
+++ tests/test_ingredient.py
@@ -3,38 +3,23 @@ from praktikum.ingredient import Ingredient
 from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
 
 
-@pytest.mark.parametrize("type, name, price, expected", [
-    (INGREDIENT_TYPE_SAUCE, "hot sauce", 100, 100),
-    (INGREDIENT_TYPE_SAUCE, "sour cream", 200, 200),
-    (INGREDIENT_TYPE_SAUCE, "chili sauce", 300, 300),
-    (INGREDIENT_TYPE_FILLING, "cutlet", 100, 100),
-    (INGREDIENT_TYPE_FILLING, "dinosaur", 200, 200),
-    (INGREDIENT_TYPE_FILLING, "sausage", 300, 300)
-])
-def test_get_price_success(type, name, price, expected):
-    ingredient = Ingredient(type, name, price)
-    assert ingredient.get_price() == expected
+types = [INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_SAUCE,
+         INGREDIENT_TYPE_FILLING, INGREDIENT_TYPE_FILLING, INGREDIENT_TYPE_FILLING]
+names = ["hot sauce", "sour cream", "chili sauce", "cutlet", "dinosaur", "sausage"]
+prices = [100, 200, 300, 100, 200, 300]
 
-@pytest.mark.parametrize("type, name, price, expected", [
-    (INGREDIENT_TYPE_SAUCE, "hot sauce", 100, "hot sauce"),
-    (INGREDIENT_TYPE_SAUCE, "sour cream", 200, "sour cream"),
-    (INGREDIENT_TYPE_SAUCE, "chili sauce", 300, "chili sauce"),
-    (INGREDIENT_TYPE_FILLING, "cutlet", 100, "cutlet"),
-    (INGREDIENT_TYPE_FILLING, "dinosaur", 200, "dinosaur"),
-    (INGREDIENT_TYPE_FILLING, "sausage", 300, "sausage")
-])
-def test_get_name_success(type, name, price, expected):
-    ingredient = Ingredient(type, name, price)
-    assert ingredient.get_name() == expected
+class TestIngredients:
+    @pytest.mark.parametrize("type, name, price, expected", zip(types, names, prices, prices))
+    def test_get_price_success(self, type, name, price, expected):
+        ingredient = Ingredient(type, name, price)
+        assert ingredient.get_price() == expected
 
-@pytest.mark.parametrize("type, name, price, expected", [
-    (INGREDIENT_TYPE_SAUCE, "hot sauce", 100, INGREDIENT_TYPE_SAUCE),
-    (INGREDIENT_TYPE_SAUCE, "sour cream", 200, INGREDIENT_TYPE_SAUCE),
-    (INGREDIENT_TYPE_SAUCE, "chili sauce", 300, INGREDIENT_TYPE_SAUCE),
-    (INGREDIENT_TYPE_FILLING, "cutlet", 100, INGREDIENT_TYPE_FILLING),
-    (INGREDIENT_TYPE_FILLING, "dinosaur", 200, INGREDIENT_TYPE_FILLING),
-    (INGREDIENT_TYPE_FILLING, "sausage", 300, INGREDIENT_TYPE_FILLING)
-])
-def test_get_type_success(type, name, price, expected):
-    ingredient = Ingredient(type, name, price)
-    assert ingredient.get_type() == expected
+    @pytest.mark.parametrize("type, name, price, expected", zip(types, names, prices, names))
+    def test_get_name_success(self, type, name, price, expected):
+        ingredient = Ingredient(type, name, price)
+        assert ingredient.get_name() == expected
+
+    @pytest.mark.parametrize("type, name, price, expected", zip(types, names, prices, types))
+    def test_get_type_success(self, type, name, price, expected):
+        ingredient = Ingredient(type, name, price)
+        assert ingredient.get_type() == expected
